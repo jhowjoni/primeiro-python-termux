d6c44c442796923a9025c554a4e08d4078e6aec2
Jonathan <jonathan.pontes@hotmail.com.br>
2019-03-24 22:19:38 -0300

    operadores artimeticos

--- value-iteration.py
+++ value-iteration.py
@@ -1,4 +1,14 @@
 n1 = int(input('Digite um numero'))
 n2 = int(input('Digite outro numero'))
 soma = n1 + n2
+divisao = n1 / n2
+subtracao =n1 - n2
+multiplicacao = n1 * n2
+divisaoInteira = n1 // n2
+exponenciacao = n1 ** n2
+raizQuadradaDeN1 = n1**(1/2)
+raizCubcaN1 = n1**(1/3)
+raizQuadradaDeN2 = n2**(1/2)
+raizCubcaN2 = n2**(1/3)
+
 print('A soma de {} com {} é igual a: {}'.format(n1, n2, soma))
