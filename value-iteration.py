50822bcc9dcfff8aa571ee81f090238a222aeaa2
Jonathan <jonathan.pontes@hotmail.com.br>
2019-03-27 20:35:04 -0300

    criacao do value inicial

--- value-iteration.py
+++ value-iteration.py
@@ -1,14 +1,19 @@
-n1 = int(input('Digite um numero'))
-n2 = int(input('Digite outro numero'))
-soma = n1 + n2
-divisao = n1 / n2
-subtracao =n1 - n2
-multiplicacao = n1 * n2
-divisaoInteira = n1 // n2
-exponenciacao = n1 ** n2
-raizQuadradaDeN1 = n1**(1/2)
-raizCubcaN1 = n1**(1/3)
-raizQuadradaDeN2 = n2**(1/2)
-raizCubcaN2 = n2**(1/3)
+# VALUE ITERATION
 
-print('A soma de {} com {} é igual a: {}'.format(n1, n2, soma))
+#estados=[1,2,3,4,5,6,7,8,9,10]
+# probabilista -0,5 ficar no mesmo lugar e -0,5 para ir para o proximo estado
+# direcoes N, S, L, O
+# meta S5 -- r(S5)= 0
+
+gama=1
+r=-1
+epson= 0.0001
+
+listaLeste=[[0,-0.5,0],[-0.5,0,-0.5],[-0.5,0,0]]
+
+for i in range(0,len(listaLeste)):
+    for j in range(0,len(listaLeste[i])):
+        t =listaLeste[i][j]
+        print(t)
+
+
