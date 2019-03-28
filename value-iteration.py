c9df23e79a2bde2802c781f48b49d2cffb5d6a2a
Jonathan <jonathan.pontes@hotmail.com>
2019-03-28 08:03:10 -0300

    implementacao ainda incompleta do value

--- value-iteration.py
+++ value-iteration.py
@@ -1,21 +1,32 @@
 # VALUE ITERATION
 
-#estados=[1,2,3,4,5,6,7,8,9,10]
-# probabilista -0,5 ficar no mesmo lugar e -0,5 para ir para o proximo estado
+# estados=[1,2,3,4,5,6,7,8,9,10]
+# probabilista 0,5 ficar no mesmo lugar e 0,5 para ir para o proximo estado
 # direcoes N, S, L, O
 # meta S5 -- r(S5)= 0
 
-gama=1
-r=-1
-epson= 0.0001
+gama = 1
+r = -1
+epson = 0.0001
 
-listaLeste=[[0,-0.5,0],[-0.5,0,-0.5],[-0.5,0,0]]
+listaLeste = [[0.5, 0.5, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0.5, 0.5, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0.5, 0.5, 0, 0, 0, 0, 0, 0]]
 
-for i in range(0,len(listaLeste)):
-    for j in range(0,len(listaLeste[i])):
-        t =listaLeste[i][j]
-        listaRetorno[i][j]  = float(t(r+(gama*1)))
-        print(listaRetorno)
-            
+listaNova = [[0]*10,[0]*10,[0]*10]
+print(listaNova)
+
+for i in range(0, len(listaLeste)):
+    for j in range(0, len(listaLeste[i])):
+        t0 = float(listaLeste[i][j])
+        t1 = float(listaLeste[i][j])
+        listaNova[i][j] = (t0 *(r + (gama*listaNova[i][j]))) + (t1 *(r + (gama*listaNova[i][j])))
+
+
+print(listaNova)
+        #print(listaNova)
+        #listaNova = t(r + (gama * 1))
+        #print(t)
+
 
 
