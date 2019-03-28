e563490276d5f0e372d9cd18fb2814f9a59b29e4
Jonathan <jonathan.pontes@hotmail.com.br>
2019-03-28 09:51:52 -0300

    testes, descoberto que a formula do calculo esta incorreta

--- value-iteration.py
+++ value-iteration.py
@@ -14,19 +14,20 @@ listaLeste = [[0.5, 0.5, 0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0.5, 0.5, 0, 0, 0, 0, 0, 0]]
 
 listaNova = [[0]*10,[0]*10,[0]*10]
+print('\n')
 print(listaNova)
+print('\n')
 
 for i in range(0, len(listaLeste)):
     for j in range(0, len(listaLeste[i])):
-        t0 = float(listaLeste[i][j])
-        t1 = float(listaLeste[i][j])
-        listaNova[i][j] = (t0 *(r + (gama*listaNova[i][j]))) + (t1 *(r + (gama*listaNova[i][j])))
+        t = listaLeste[i][i]
+        tn = listaLeste[i][j]
+        print('t :', t) 
+        print('tn :', tn)
+        listaNova[i][j] = (t *(r + (gama*listaNova[i][i]))) + (tn *(r + (gama*listaNova[i][j])))
 
 
 print(listaNova)
-        #print(listaNova)
-        #listaNova = t(r + (gama * 1))
-        #print(t)
 
 
 
